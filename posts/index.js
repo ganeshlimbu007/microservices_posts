482b37545b9c8758ba4646e7eaca848f40996f60
ganeshlimbu007 <57285671+ganeshlimbu007@users.noreply.github.com>
2025-11-10 11:35:33 +0545

    basic posts setup

---
+++ posts/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const posts = {};
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  const post = { id, title };
+  posts[id] = post;
+  res.status(201).send(post);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  delete posts[id];
+  res.status(200).send({ message: "Post deleted successfully" });
+});
+
+app.listen(4000, () => {
+  console.log("Posts service listening on port 4000");
+});
